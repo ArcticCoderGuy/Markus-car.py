397d9d91d7b78af2966564f01695ea849a3fb9de
Markus Kaprio <markus.kaprio@edunord.se>
2025-12-02 14:43:50 +0200

    Creating the car object

---
+++ Markus_car.py
@@ -0,0 +1,12 @@
+class Car: 
+    """A class representing a car."""
+
+    def _init_(self,make, model, year, price, horsepower):
+        """Initialize the car's attributes."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.price = price
+        self.horsepower = horsepower
+
+git 
